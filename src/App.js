8a1a2c891de6a0d4a7d6968ea8426f556d706db2
abdelghani annani <abdelghani.annani@osthus.com>
2020-03-02 16:05:33 +0100

    kickoff day

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Sidebar from './components/Sidebar.jsx';
+import Submission from './components/Submission.jsx';
+import "./components/css.css"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+      <div className="row">
+        <Sidebar/>
+        </div>
+        <div className="row form-side">
+        <Submission/>
+        </div>
+      </div>
     </div>
   );
 }
